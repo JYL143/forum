84071cc9b952c810c8583ed7573078d7006350fd
jyl <1432364557@qq.com>
2020-03-04 15:03:56 +0800

    测试idea快捷提交git

--- src/main/java/com/jyl/springboot_forum/controller/IndexController.java
+++ src/main/java/com/jyl/springboot_forum/controller/IndexController.java
@@ -7,14 +7,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloController {
+public class IndexController {
 
 
-
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    /**
+     * 主页面
+     * @return
+     */
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 
 }
